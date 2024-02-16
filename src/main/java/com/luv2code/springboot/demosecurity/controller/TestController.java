5d20881b39f9a97b21ce8d814fdfc83b874023c7
nitish093 <nknitish6060@gmail.com>
2024-02-16 12:52:15 +0530

    New Test Changes related to learning.

--- src/main/java/com/luv2code/springboot/demosecurity/controller/TestController.java
+++ src/main/java/com/luv2code/springboot/demosecurity/controller/TestController.java
@@ -1,7 +1,16 @@
 package com.luv2code.springboot.demosecurity.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class TestController {
+
+    //Changes for learning GIT
+    @GetMapping("details")
+    public void testMethod1(@PathVariable("details") String details)
+    {
+        System.out.println("Test");
+    }
 }
